352cc1d14105f51113e37c41092aa28119c4eadb
Gina Zhou <ginayizhou@gmail.com>
2015-01-26 15:55:04 -0800

    merged

--- VendableClient/platforms/ios/www/js/app.js
+++ VendableClient/platforms/ios/www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var Vendable=angular.module('Vendable',['ionic']);
 
-.run(function($ionicPlatform) {
+Vendable.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -17,3 +17,218 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+Vendable.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('home',{
+    url: '/',
+    templateUrl: 'templates/home.html'
+  });
+
+  $stateProvider.state('index',{
+    url: '/index',
+    templateUrl: 'templates/index.html'
+  });
+
+  $stateProvider.state('new_list', {
+    url: '/new_list',
+    templateUrl: 'templates/new_list.html'
+  })
+
+  $stateProvider.state('all_list', {
+    url: '/index',
+    templateUrl: 'templates/index.html'
+  })
+
+});
+
+
+
+Vendable.factory('searchItemsService',function($http){
+      return{
+            scan:function(keyWord){
+                  console.log("u")
+            return $http.get('http://localhost:9393')
+                    .then(function(response){
+                      return response.data;
+                    })
+            }
+            }
+});
+
+Vendable.factory('Lists',function(){
+  return{
+    all:function(){
+      var listsString=window.localStorage['lists'];
+      if(listsString){
+        return angular.fromJson(listsString);
+      }
+      return [];
+    },
+
+    save:function(listName){
+      window.localStorage['lists']=angular.toJson(listName);
+    },
+
+    newList:function(listName,id){
+      return{
+        id:id,
+        title:listName,
+        items:[]
+      }
+    },
+
+    removeList:function(listObject){
+      console.log("I m here");
+      var list=angular.fromJson(window.localStorage['lists']);
+      var updatedList=list.filter(function(el){
+        return el.title !== listObject.title
+      })
+      window.localStorage['lists']=angular.toJson(updatedList);
+    },
+
+    getLastActiveList:function(){
+      return parseInt(window.localStorage['lastActiveList']) || 0;
+      //return index number 0 if no prior active list
+    },
+
+    setLastActiveList:function(idx){
+      window.localStorage['lastActiveList']=idx;
+    }
+
+  }
+})
+
+Vendable.controller('VendableCtrl',
+  // ['$scope','$http','$ionicModal',
+    function($scope,searchItemsService,Lists,$ionicModal,$ionicSideMenuDelegate){
+      console.log($scope.activeList)
+      
+      $scope.lists=Lists.all();//This is an array
+
+      var createList=function(listName){
+        var id = function(){
+          console.log($scope.lists.length)
+          if($scope.lists.length === 0){
+            return 0
+          }else{
+          return $scope.lists[$scope.lists.length-1].id+1
+        }
+        };
+        var newList=Lists.newList(listName,id());
+        $scope.lists.push(newList);
+        Lists.save($scope.lists);
+        $scope.selectList(newList);
+        console.log(newList)
+      }
+
+      $scope.activeList=$scope.lists[Lists.getLastActiveList()];
+      console.log($scope.activeList);
+
+
+      $scope.addList=function(){
+        var listName=prompt('Give me a Name');
+        if (listName){
+          createList(listName);
+        }
+      };
+
+      $scope.selectList=function(list){
+        $scope.activeList=list;
+        Lists.setLastActiveList(list.id);
+        $ionicSideMenuDelegate.toggleLeft(false);
+      }
+
+      $scope.deleteList=function(list){
+        Lists.removeList(list);
+        $scope.selectList($scope.lists[0]);
+      }
+
+      $scope.toggleLeft = function() {
+        console.log("here");
+        $ionicSideMenuDelegate.toggleLeft()
+      };
+
+      $ionicModal.fromTemplateUrl("templates/search.html",{
+        scope: $scope,
+        animation: 'slide-in-up'
+      }).then(function(modal){
+        $scope.modal = modal //This change the modal of the scope
+      });
+//-------------------------------------------
+      // $ionicModal.fromTemplateUrl("templates/map_modal.html", {
+      //    scope: $scope,
+      //    animation: 'slide-in-up'
+      //  }).then(function(modal){
+      //    $scope.modal = modal
+      //  })
+
+       $scope.openMap = function() {
+          $scope.modal.show();
+          if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(drawMap)
+          }
+          else {
+            $('#message').text("Geolocation not supported")
+          }
+        }
+
+        $scope.closeModal = function(){
+          $scope.modal.hide();
+        }
+
+
+        // $scope.getLocation = function() {
+        //   if (navigator.geolocation) {
+        //     navigator.geolocation.getCurrentPosition(drawMap)
+        //   }
+        //   else {
+        //     $('#message').text("Geolocation not supported")
+        //   }
+        // }
+        var drawMap = function(position){
+          var map = new GMaps({
+            div: '#mapG',
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          });
+          console.log(position.coords.latitude, position.coords.longitude )
+         $http.get('http://192.168.060:3000').success(function(response){
+            console.log(response)
+         })
+        }
+//-------------------------------------------------
+      $scope.openSearchModal = function(){
+        $scope.modal.show()
+      };
+
+      $scope.closeSearchModal = function(){
+        $scope.modal.hide()
+      }
+
+
+      $scope.data={};
+      $scope.items={};
+
+      $scope.search=function(){
+        searchItemsService.scan($scope.data.keyWord).then(function(response){
+          $scope.items=response
+        });
+      }
+
+      $scope.addItem=function(item){
+        $scope.activeList.items.push(item)
+
+        console.log($scope.lists);
+        Lists.save($scope.lists);
+      }
+
+      $scope.deleteItem=function(item){
+        var idx = $scope.basket.indexOf(item);
+        $scope.basket.splice(idx,1);
+      }
+}
+// ]
+);
+
