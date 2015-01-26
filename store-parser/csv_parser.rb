bef249815560b436655878f8406e97bbf9685582
briemarie <thebriemarie@gmail.com>
2015-01-25 16:32:29 -0800

    Add all stores into giant hash, parse for duplicate items

--- store-parser/csv_parser.rb
+++ store-parser/csv_parser.rb
@@ -1,30 +1,88 @@
 require 'CSV'
+require 'json'
 
-fruit = CSV.readlines('safeway-best.csv')
-items = []
-name = []
-price = []
+store_hash = {'safeway.csv' => 'safeway', 'costco.csv' => 'costco', 'whole_foods.csv' => 'whole_foods', 'fresh_n_easy.csv' => 'fresh_n_easy'}
 
+giant_array = []
 
-fruit.each do |row|
-  row.to_s.strip
-  items << row[3]
+store_hash.each do |k, v|
+
+  current_store = CSV.readlines(k)
+  items = []
+  name = []
+  price = []
+
+  current_store.each do |row|
+    items << row
+  end
+
+  items.each do |item|
+    div_item = item.to_s.split('$')
+    name << div_item[0]
+    price << div_item[1]
+  end
+
+  name.each do |item|
+    new_item = item.to_s
+    items << new_item.slice!("\\n\\n")
+    items << new_item.slice!("\\n")
+    items << new_item.gsub!("[", '')
+    items << new_item.gsub!("\"", '')
+    items << new_item.strip!
+  end
+
+  price.each do |item|
+    new_item = item.to_s
+    items << new_item.slice!("\\n\\n")
+    items << new_item.gsub!("]", '')
+    items << new_item.gsub!("\"", '')
+    items << new_item.strip!
+  end
+
+  object = name.zip(price)
+
+  obj_hash = {}
+
+  object.map do |item|
+    new_item = item[0].split("  ")
+    item[0] = new_item.shift().sub("\\", "")
+
+    new_item = item[1].split(" ")
+    item[1] = new_item.shift().sub("\\n", "")
+  end
+
+  object.each do |item|
+    new_obj = Hash[ 'type', item[0],  'store', [v, item[1]] ]
+    giant_array.push(new_obj)
+  end
+end
+
+ p giant_array.length
+
+ similar_array = []
+
+giant_array.each do |item|
+
+  giant_array.each do |current_item|
+    if item['type'].downcase.include?(current_item['type'].downcase) && item['store'][0] != current_item['store'][0]
+      similar_array.push(item)
+      similar_array.push(current_item)
+    end
+
+  end
 
 end
 
-# name.delete_if{|i| i == ""}
-# price.delete_if{|i| i == ""}
+p similar_array
 
-# price.each do |string|
-#   string.slice!("\n")
-#   string.slice!("<strong>·</strong>\n")
-#   string.gsub!(/\s/,'')
-# end
 
-# print name
-# print price
 
-p items
+
+
+
+
+
+
 
 
 
