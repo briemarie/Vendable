6b444bd6b00f23056285ecf426e49e8529ef12a6
Gina Zhou <ginayizhou@gmail.com>
2015-01-26 12:37:52 -0800

    added new List function

--- VendableClient/www/js/app.js
+++ VendableClient/www/js/app.js
@@ -18,13 +18,136 @@ Vendable.run(function($ionicPlatform) {
   });
 })
 
+Vendable.config(function($stateProvider, $urlRouterProvider){
+  $urlRouterProvider.otherwise('/')
+
+  $stateProvider.state('home',{
+    url: '/',
+    templateUrl: 'template/home.html'
+  });
+
+  $stateProvider.state('index',{
+    url: '/index',
+    templateUrl: 'template/index.html'
+  });
+
+  $stateProvider.state('new_list', {
+    url: '/new_list',
+    templateUrl: 'template/new_list.html'
+  })
+
+  $stateProvider.state('all_list', {
+    url: '/all_list',
+    templateUrl: 'template/all_list.html'
+  })
+
+});
+
+
+
+Vendable.factory('searchItemsService',function($http){
+      return{
+            scan:function(keyWord){
+                  console.log("u")
+            return $http.get('http://localhost:9393')
+                    .then(function(response){
+                      return response.data;
+                    })
+            }
+            }
+});
+
+Vendable.factory('Lists',function(){
+  return{
+    all:function(){
+      console.log("shit");
+      var listsString=window.localStorage['lists'];
+      if(listsString){
+        return angular.fromJson(listsString);
+      }
+      return [];
+    },
+
+    save:function(listName){
+      window.localStorage['lists']=angular.toJson(listName);
+    },
+
+    newList:function(listName,id){
+      return{
+        id:id,
+        title:listName,
+        items:[]
+      }
+    },
+
+    removeList:function(listObject){
+      console.log("I m here");
+      var list=angular.fromJson(window.localStorage['lists']);
+      var updatedList=list.filter(function(el){
+        return el.title !== listObject.title
+      })
+      window.localStorage['lists']=angular.toJson(updatedList);
+    },
+
+    getLastActiveList:function(){
+      return parseInt(window.localStorage['lastActiveList']) || 0;
+      //return index number 0 if no prior active list
+    },
+
+    setLastActiveList:function(id){
+      window.localStorage['lastActiveList']=id;
+    }
+
+  }
+})
+
 Vendable.controller('VendableCtrl',
-  ['$scope','$http','$ionicModal',
-    function($scope,$http,$ionicModal){
+  // ['$scope','$http','$ionicModal',
+    function($scope,searchItemsService,Lists,$ionicModal,$ionicSideMenuDelegate){
 
       $scope.basket=[];
+      $scope.lists=Lists.all();//This is an array
 
-      $ionicModal.fromTemplateUrl("search-item-modal.html",{
+      var createList=function(listName){
+        var id = function(){
+          if($scope.lists.length === 0){
+            return 0
+          }
+          return $scope.lists[$scope.lists.length-1].id
+        };
+        var newList=Lists.newList(listName,id());
+        $scope.lists.push(newList);
+        Lists.save($scope.lists);
+        $scope.selectList(newList);
+        console.log(newList)
+      }
+
+      $scope.activeList=$scope.lists[Lists.getLastActiveList()];
+
+      $scope.addList=function(){
+        var listName=prompt('Give me a Name');
+        if (listName){
+          createList(listName);
+        }
+      };
+
+      $scope.selectList=function(list){
+        $scope.activeList=list;
+        Lists.setLastActiveList(list.id);
+        $ionicSideMenuDelegate.toggleLeft(false);
+      }
+
+      $scope.deleteList=function(list){
+        Lists.removeList(list);
+        $scope.selectList($scope.lists[0]);
+      }
+
+      $scope.toggleLeft = function() {
+        console.log("here");
+        $ionicSideMenuDelegate.toggleLeft()
+      };
+
+      $ionicModal.fromTemplateUrl("template/search.html",{
         scope: $scope,
         animation: 'slide-in-up'
       }).then(function(modal){
@@ -43,15 +166,15 @@ Vendable.controller('VendableCtrl',
       $scope.data={};
       $scope.items={};
 
-      $scope.scan=function(){
-          $http.get('https://sleepy-scrubland-3514.herokuapp.com/food/'+$scope.data.keyWord)
-            .success(function(data){
-              $scope.items=data;
-            });
-      };
+      $scope.search=function(){
+        searchItemsService.scan($scope.data.keyWord).then(function(response){
+          $scope.items=response
+        });
+      }
 
       $scope.addItem=function(item){
-        $scope.basket.push(item); 
+        console.log(Lists.getLastActiveList());
+        $scope.lists[Lists.getLastActiveList()].items.push(item); 
         console.log(item)
       }
 
@@ -59,5 +182,9 @@ Vendable.controller('VendableCtrl',
         var idx = $scope.basket.indexOf(item);
         $scope.basket.splice(idx,1);
       }
-}]);
+}
+// ]
+);
+
+console.log("yeah")
 
