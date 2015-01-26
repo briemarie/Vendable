2396ea2eea9c46fb131d6f73846b1211024fe927
juan267 <j.gomez267@gmail.com>
2015-01-26 12:17:44 -0800

    added map

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var vendable = angular.module('starter', ['ionic'])
+var vendable = angular.module('Vendable', ['ionic'])
 
 vendable.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/')
@@ -23,5 +23,48 @@ vendable.config(function($stateProvider, $urlRouterProvider){
     templateUrl: 'templates/all_list.html'
   })
 })
+
+vendable.controller('VendableCtrl',
+  // ['$scope','$ionicModal','$http',
+  function($scope,$http, $ionicModal){
+
+  $ionicModal.fromTemplateUrl("templates/map_modal.html", {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal){
+    $scope.modal = modal
+  })
+
+  $scope.openMap = function() {
+    $scope.modal.show();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(drawMap)
+    }
+    else {
+      $('#message').text("Geolocation not supported")
+    }
+  }
+
+  $scope.closeModal = function(){
+    $scope.modal.hide();
+  }
+
+  // $scope.getLocation = function() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(drawMap)
+  //   }
+  //   else {
+  //     $('#message').text("Geolocation not supported")
+  //   }
+  // }
+  var drawMap = function(position){
+    var map = new GMaps({
+      div: '#mapG',
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+    });
+    console.log(position.coords.latitude, position.coords.longitude )
+  }
+});
 
 
