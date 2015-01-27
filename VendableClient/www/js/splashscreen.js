05961fee52efd83e7427d69e07b9822daf1f2c5e
briemarie <thebriemarie@gmail.com>
2015-01-26 16:02:18 -0800

    add module controller for splashscreen plugin

--- VendableClient/www/js/splashscreen.js
+++ VendableClient/www/js/splashscreen.js
@@ -1,3 +1,9 @@
+Vendable.controller('VendableCtrl', function($scope, $cordovaSplashscreen) {
+
+  $cordovaSplashscreen.show();
+
+});
+
 Vendable.run(function($cordovaSplashscreen) {
   setTimeout(function() {
     $cordovaSplashscreen.hide()
