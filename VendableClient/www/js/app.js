0ae4b4fd8ad2e01bc5bfb0aafb82638f21de4c24
briemarie <thebriemarie@gmail.com>
2015-01-26 18:07:19 -0800

    Add ngCordova to app dependendency

--- VendableClient/www/js/app.js
+++ VendableClient/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var Vendable=angular.module('Vendable',['ionic']);
+var Vendable=angular.module('Vendable',['ionic', 'ngCordova']);
 
 Vendable.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -104,7 +104,7 @@ Vendable.controller('VendableCtrl',
   // ['$scope','$http','$ionicModal',
     function($scope,searchItemsService,Lists,$ionicModal,$ionicSideMenuDelegate){
       console.log($scope.activeList)
-      
+
       $scope.lists=Lists.all();//This is an array
 
       var createList=function(listName){
